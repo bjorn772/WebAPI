040b898485f0c42d4272f34965abe950e7cddeb7
Björn <bjorn7681@gmail.com>
2021-03-15 12:39:29 +0100

    Add project files.

---
+++ WebAPI/Controllers/ProductController.cs
@@ -0,0 +1,171 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using WebAPI.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace WebAPI.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _env;
+
+        public ProductController(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            _configuration = configuration;
+            _env = env;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                    select ProductId, ProductName, Description,
+                    convert(varchar(10),DateAdded,120) as DateAdded
+                    ,PhotoFileName
+                    from dbo.Product
+                    ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppConn");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+        [HttpPost]
+        public JsonResult Post(Product prod)
+        {
+            string query = @"
+                     insert into dbo.Product 
+                     (ProductName, Description, DateAdded, PhotoFileName)
+                     values 
+                     (
+                      '" + prod.ProductName + @"',
+                      '" + prod.Description + @"',
+                      '" + prod.DateAdded + @"',
+                      '" + prod.PhotoFileName + @"'
+                     )
+                     ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppConn");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Added Succesfully");
+        }
+
+        [HttpPut]
+        public JsonResult Put(Product prod)
+        {
+            string query = @"
+                    update dbo.Product set 
+                    ProductName = '" + prod.ProductName + @"'
+                    ,Description = '" + prod.Description + @"'
+                    ,DateAdded = '" + prod.DateAdded + @"'
+                    where ProductId = " + prod.ProductId + @" 
+                    ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppConn");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Updates Succesfully");
+        }
+
+        [Route("SaveFile")]
+        [HttpPost]
+        public JsonResult SaveFile()
+        {
+            try
+            {
+                var httpRequest = Request.Form;
+                var postedFile = httpRequest.Files[0];
+                string filename = postedFile.FileName;
+                var physicalPath = _env.ContentRootPath + "/Photos/" + filename;
+
+                using (var stream = new FileStream(physicalPath, FileMode.Create))
+                {
+                    postedFile.CopyTo(stream);
+                }
+
+                return new JsonResult(filename);
+            }
+            catch (Exception)
+            {
+
+                return new JsonResult("anonymous.png");
+            }
+        }
+
+        [Route("GetAllProducts")]
+        public JsonResult GetAllProducts()
+        {
+            string query = @"
+                    select ProductName from dbo.Product
+                    ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppConn");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+    }
+}
---
+++ WebAPI/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public string DateAdded { get; set; }
+        public string PhotoFileName { get; set; }
+    }
+}
